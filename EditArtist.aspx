3c3de2bf7c1160505bccb3f0cb51fe41a750b3ac
Saukonoja <timo.saukonoja@gmail.com>
2016-11-13 00:10:37 +0200

    Selection and Adding for artist

--- EditArtist.aspx
+++ EditArtist.aspx
@@ -1,21 +1,30 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="EditArtist.aspx.cs" Inherits="EditArtist" %>
+﻿<%@ Page Title="" EnableEventValidation="false" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="EditArtist.aspx.cs" Inherits="EditArtist" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="body" runat="Server">
     <br />
     <h1 id="pageHeader">Edit or Add new</h1>
-        <asp:GridView ID="gvEditArtist" CssClass="query" runat="server">
-        </asp:GridView>
+
+    <asp:GridView ID="gvEditArtist" AutoGenerateSelectButton="true" SelectedIndex="0" OnSelectedIndexChanged="gvEditArtist_SelectedIndexChanged" OnSelectedIndexChanging="gvEditArtist_SelectedIndexChanging" CssClass="query" runat="server">
+
+    </asp:GridView>
 
     <div class="editFields">
         <p>Name</p>
-        <asp:TextBox ID="tbArtistName" runat="server"></asp:TextBox> <br />
+        <asp:TextBox ID="txtArtistName" runat="server"></asp:TextBox>
+        <br />
         <p>Select year</p>
-        <asp:DropDownList ID="ddlSelectYear" runat="server"></asp:DropDownList> <br />
+        <asp:DropDownList ID="ddlSelectYear" runat="server"></asp:DropDownList>
+        <br />
         <p>Select country</p>
-        <asp:DropDownList ID="ddlSelectCountry" runat="server"></asp:DropDownList> <br />
-        <asp:Button ID="btnAdd" Text="Add artist" runat="server" /> <br />
-        <asp:Button ID="btnSave" Text="Save changes" runat="server" /> <br />
-        <asp:Button ID="btnDelete" Text="Delete artist" runat="server" />
+        <asp:DropDownList ID="ddlSelectCountry" runat="server"></asp:DropDownList>
+        <br />
+        <asp:Button ID="btnAdd" Text="Add artist" OnClick="btnAdd_Click" runat="server" />
+        <br />
+        <asp:Button ID="btnSave" Text="Save changes" OnClick="btnSave_Click" runat="server" />
+        <br />
+        <asp:Button ID="btnDelete" Text="Delete artist" OnClick="btnDelete_Click" runat="server" />
+
+        <asp:Label ID="lblMessages" Text="Null" runat="server"></asp:Label>
     </div>
 
 </asp:Content>
--- EditArtist.aspx.cs
+++ EditArtist.aspx.cs
@@ -5,9 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using MusicDatabase;
+using System.Data;
 
 public partial class EditArtist : System.Web.UI.Page
 {
+    int selectedId = 0;
     protected void Page_Load(object sender, EventArgs e)
     {
         IniEditArtist();
@@ -24,4 +26,85 @@ public partial class EditArtist : System.Web.UI.Page
     }
 
 
+    protected void gvEditArtist_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (gvEditArtist.SelectedIndex > -1)
+        {
+            try
+            {
+                GridViewRow row = gvEditArtist.SelectedRow;
+                txtArtistName.Text = row.Cells[1].Text;
+                ddlSelectYear.Text = row.Cells[2].Text;
+                ddlSelectCountry.Text = row.Cells[3].Text;
+                //selectedId = int.Parse(row.Cells[4].Text);
+                selectedId = Convert.ToInt32(row.Cells[4].Text);
+                lblMessages.Text = selectedId.ToString();
+                
+           
+            }
+            catch (Exception ex)
+            {
+                lblMessages.Text = ex.Message.ToString();
+            }
+        }
+    }
+
+    protected void gvEditArtist_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+    {
+        GridViewRow row = gvEditArtist.Rows[e.NewSelectedIndex];
+    }
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        if (txtArtistName.Text != "")
+        {
+            try
+            {
+                string name = txtArtistName.Text;
+                int year = int.Parse(ddlSelectYear.Text);
+                string country = ddlSelectCountry.Text;
+                Artist.AddArtist(name, country, year);
+                IniEditArtist();
+            }
+            catch (Exception ex)
+            {
+
+                lblMessages.Text = ex.Message.ToString();
+            }
+        }
+        
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Artist.DeleteArtist(selectedId);
+            IniEditArtist();
+        }
+        catch (Exception ex)
+        {
+
+            lblMessages.Text = ex.Message.ToString();
+        }
+        
+    }
+
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string name = txtArtistName.Text;
+            int year = Convert.ToInt32(ddlSelectYear.Text);
+            string country = ddlSelectCountry.ToString();
+            Artist.UpdateArtist(89, name, country, year);
+            IniEditArtist();
+        }
+        catch (Exception ex)
+        {
+
+            lblMessages.Text = ex.Message.ToString();
+        }
+      
+    }
 }
